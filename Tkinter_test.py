441d21709c57ffbb64de73b54215be4c73dd9f7c
Peyton Cleveland-Shell <peytonjcs05@gmail.com>
2026-04-24 00:33:37 -0700

    Attemted to add Sectioned menus to top of screen.

--- Tkinter_test.py
+++ Tkinter_test.py
@@ -26,18 +26,36 @@ except Exception as e:
 size = os.get_terminal_size()
 
 def WindowStartForGen():
+    
+    
     #create window
     root = Tk()
     
+    
+    editorMenu = Menu(root)
+    root.config(menu=editorMenu)
+
+    def our_command():
+        pass
+    
+    file_Menu = Menu(editorMenu)
+    editorMenu.add_cascade(Label="File", menu=file_Menu)
+    file_Menu.add_command(Label="New...", command=root.our_command)
+    file_Menu.add_separator()
+    file_Menu.add_command(Label="Exit", command=root.quit)
+
 
     # Name of Window
     root.title("Card Generator Definer")
 
+
     # Window Size
     root.geometry("960x540")
 
+
     # Locationary Variables
     IntroTitle = [110, 40]
+
 
     # Prints Text to Window
     label = Label(root, text = "Welcome to the Digital Card Creator.")
@@ -45,27 +63,48 @@ def WindowStartForGen():
     label = Label(root, text = "This Code is designed to aid and help in the creation of Trading Card Creation for digital games or for eventual printing.")
     label.place(x = IntroTitle[0], y = (IntroTitle[1] + IntroTitle[1]*1))
 
+
     # Copy Names of files from Deck_List
     deck_options = []
     for deck in os.listdir("Deck_List"):
             # Prints file names present Deck_List folder
-            deck_options.append(deck) # add to list
+            deck_options.append(deck)
+    # Adds '+ New Deck' button to Deck_List, allowing for adding of decks.
     deck_options.append("+ New Deck")
+
+    def editorsMenu(y=False, n=False):
+        if y == True:
+            selectDeck = Label(root, text="Opening the Deck folder.").pack()
+        elif n == True:
+            selectDeck = Label(root, text="Cancelling opening of the Deck folder.").pack()
 
     def selected(event):
         #selectDeck = Label(root, text=clicked.get()).pack()
         if clicked.get() == "+ New Deck":
-            selectDeck = Label(root, text="Creating new Deck folder.").pack()
+            selectDeck = Label(root, text="Creating a new Deck folder.").pack()
         else:
-            selectDeck = Label(root, text=f'The Deck selected was {clicked.get().pack()}.').pack()
+            selectDeck = Label(root, text=f'The Deck selected was {clicked.get()}. Would you like to confirm?').pack()
+            confirmDeck = Button(root, text="Confirm Deck", command=editorsMenu)
+            confirmDeck.pack()
+
     
     clicked = StringVar()
     clicked.set(deck_options[0])
 
+    # Creates drop down menu for interactions
     drop = OptionMenu(root, clicked, *deck_options, command=selected)
     drop.pack(pady=20)
-
-    # myButton = Button(root, text"", command=selected)
+    
+    
+    '''
+    deck_Select = ttk.Combobox(root, value=deck_options)
+    deck_Select.current(0)
+    deck_Select.bind("<<ComboboxSelected>>", selected)
+    deck_Select.pack()
+    '''
+    
+    
+    
 
     # Start Event Loop
     root.mainloop()
